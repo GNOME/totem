e099b50a6ea1821dddb26c3b9acdffe4fc01f116
Bastien Nocera <hadess@hadess.net>
2002-12-23 12:28:36 +0000

    change the (C) to a proper copyright sign
    2002-12-23  Bastien Nocera  <hadess@hadess.net>
    
            * src/totem.c: (on_about1_activate): change the (C) to a proper
    	copyright sign

--- src/totem.c
+++ src/totem.c
@@ -1196,7 +1196,7 @@ on_about1_activate (GtkButton *button, gpointer user_data)
 	}
 
 	about = gnome_about_new(_("Totem"), VERSION,
-			_("Copyright (c) 2002 Bastien Nocera"),
+			_("Copyright \xc2\xa9 2002 Bastien Nocera"),
 			_("Movie Player (based on the Xine libraries)"),
 			(const char **)authors,
 			(const char **)documenters,
