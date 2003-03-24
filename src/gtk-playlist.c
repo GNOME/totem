947767915a34a298c129c182e1532c46bd9f24c5
Bastien Nocera <hadess@hadess.net>
2003-03-24 00:34:42 +0000

    removed added
    2003-03-24  Bastien Nocera  <hadess@hadess.net>
    
            * data/Makefile.am:
    	* data/totem_logo.mpv: removed
    	* data/totem_logo.png: added
    	* src/Makefile.am:
    	* src/gtk-playlist.c: (gtk_playlist_add_mrl): use a PNG logo now that
    	xine supports it, it's easier to generate

--- src/gtk-playlist.c
+++ src/gtk-playlist.c
@@ -1148,7 +1148,8 @@ gtk_playlist_add_mrl (GtkPlaylist *playlist, const char *mrl,
 
 	if (strncmp ("audio/", mimetype, 6) != 0
 			&& strncmp ("video/", mimetype, 6) != 0
-			&& strncmp ("application/x-ogg", mimetype, 17) != 0)
+			&& strncmp ("application/x-ogg", mimetype, 17) != 0
+			&& strncmp ("image/png", mimetype, 9) != 0)
 	{
 		D("trying to add '%s' with mimetype '%s'",
 				mrl, mimetype);
