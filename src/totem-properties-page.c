a514c4a34589c82cef6d656a1d939b1b9a04187d
Bastien Nocera <hadess@hadess.net>
2003-05-06 22:55:09 +0000

    implemented the widget creation
    2003-05-06  Bastien Nocera  <hadess@hadess.net>
    
            * src/totem-properties-page.c: (totem_properties_page_init),
    	(totem_properties_page_finalize): implemented the widget creation

--- src/totem-properties-page.c
+++ src/totem-properties-page.c
@@ -104,89 +104,36 @@ make_bold_label(const gchar *message)
 static void
 totem_properties_page_init(TotemPropertiesPage *self)
 {
+	GtkWidget *vbox;
 	BonoboPropertyBag *pb;
-#if 0
-	GtkWidget *label, *textview, *swin;
+	GError *err = NULL;
+	char *filename;
 
-	self->location = NULL;
+	self->bvw = BACON_VIDEO_WIDGET (bacon_video_widget_new
+			(-1, -1, TRUE, &err));
+	//FIXME
 
-	self->table = gtk_table_new(5, 2, FALSE);
-	gtk_table_set_row_spacings(GTK_TABLE(self->table), 3);
-	gtk_table_set_col_spacings(GTK_TABLE(self->table), 3);
-	gtk_container_set_border_width(GTK_CONTAINER(self->table), 3);
+	filename = g_build_filename (G_DIR_SEPARATOR_S, DATADIR,
+			"totem", "properties.glade", NULL);
+	self->xml = glade_xml_new (filename, "vbox1", NULL);
+	g_free (filename);
 
-	label = make_bold_label(_("Name:"));
-	gtk_table_attach(GTK_TABLE(self->table), label, 0,1, 0,1,
-			GTK_FILL, GTK_FILL, 0, 0);
-	gtk_widget_show(label);
-	self->pkg_name = gtk_label_new(_("<none>"));
-	gtk_label_set_selectable(GTK_LABEL(self->pkg_name), TRUE);
-	gtk_misc_set_alignment(GTK_MISC(self->pkg_name), 0.0, 0.5);
-	gtk_table_attach(GTK_TABLE(self->table), self->pkg_name, 1,2, 0,1,
-			GTK_FILL|GTK_EXPAND, GTK_FILL, 0, 0);
-	gtk_widget_show(self->pkg_name);
+	if (self->xml == NULL)
+		return;
 
-	label = make_bold_label(_("Version:"));
-	gtk_table_attach(GTK_TABLE(self->table), label, 0,1, 1,2,
-			GTK_FILL, GTK_FILL, 0, 0);
-	gtk_widget_show(label);
-	self->pkg_version = gtk_label_new("");
-	gtk_label_set_selectable(GTK_LABEL(self->pkg_version), TRUE);
-	gtk_misc_set_alignment(GTK_MISC(self->pkg_version), 0.0, 0.5);
-	gtk_table_attach(GTK_TABLE(self->table), self->pkg_version, 1,2, 1,2,
-			GTK_FILL|GTK_EXPAND, GTK_FILL, 0, 0);
-	gtk_widget_show(self->pkg_version);
+	vbox = glade_xml_get_widget (self->xml, "vbox1");
+	gtk_widget_show (vbox);
 
-	label = make_bold_label(_("Group:"));
-	gtk_table_attach(GTK_TABLE(self->table), label, 0,1, 2,3,
-			GTK_FILL, GTK_FILL, 0, 0);
-	gtk_widget_show(label);
-	self->pkg_group = gtk_label_new("");
-	gtk_label_set_selectable(GTK_LABEL(self->pkg_group), TRUE);
-	gtk_misc_set_alignment(GTK_MISC(self->pkg_group), 0.0, 0.5);
-	gtk_table_attach(GTK_TABLE(self->table), self->pkg_group, 1,2, 2,3,
-			GTK_FILL|GTK_EXPAND, GTK_FILL, 0, 0);
-	gtk_widget_show(self->pkg_group);
+	bonobo_control_construct (BONOBO_CONTROL (self), vbox);
 
-	label = make_bold_label(_("Install date:"));
-	gtk_table_attach(GTK_TABLE(self->table), label, 0,1, 3,4,
-			GTK_FILL, GTK_FILL, 0, 0);
-	gtk_widget_show(label);
-	self->pkg_installdate = gtk_label_new("");
-	gtk_label_set_selectable(GTK_LABEL(self->pkg_installdate), TRUE);
-	gtk_misc_set_alignment(GTK_MISC(self->pkg_installdate), 0.0, 0.5);
-	gtk_table_attach(GTK_TABLE(self->table), self->pkg_installdate, 1,2, 3,4,
-			GTK_FILL|GTK_EXPAND, GTK_FILL, 0, 0);
-	gtk_widget_show(self->pkg_installdate);
-
-	swin = gtk_scrolled_window_new(NULL, NULL);
-	gtk_scrolled_window_set_policy(GTK_SCROLLED_WINDOW(swin),
-			GTK_POLICY_AUTOMATIC, GTK_POLICY_AUTOMATIC);
-	gtk_scrolled_window_set_shadow_type(GTK_SCROLLED_WINDOW(swin),
-			GTK_SHADOW_IN);
-	gtk_table_attach(GTK_TABLE(self->table), swin, 0,2, 4,5,
-			GTK_FILL|GTK_EXPAND, GTK_FILL|GTK_EXPAND, 0, 0);
-	gtk_widget_show(swin);
-
-	textview = gtk_text_view_new();
-	self->pkg_description = gtk_text_view_get_buffer(GTK_TEXT_VIEW(textview));
-	gtk_text_view_set_editable(GTK_TEXT_VIEW(textview), FALSE);
-	gtk_text_view_set_wrap_mode(GTK_TEXT_VIEW(textview), GTK_WRAP_WORD);
-	gtk_container_add(GTK_CONTAINER(swin), textview);
-	gtk_widget_show(textview);
-
-	gtk_widget_show(self->table);
-
-	bonobo_control_construct(BONOBO_CONTROL(self), self->table);
-#endif
 	pb = bonobo_property_bag_new (
 			(BonoboPropertyGetFn) get_property,
 			(BonoboPropertySetFn) set_property, self);
-	bonobo_property_bag_add(pb, "URI", PROP_URI, BONOBO_ARG_STRING,
+	bonobo_property_bag_add (pb, "URI", PROP_URI, BONOBO_ARG_STRING,
 			NULL, _("URI currently displayed"), 0);
-	bonobo_control_set_properties(BONOBO_CONTROL(self),
-			BONOBO_OBJREF(pb), NULL);
-	bonobo_object_release_unref(BONOBO_OBJREF(pb), NULL);
+	bonobo_control_set_properties (BONOBO_CONTROL (self),
+			BONOBO_OBJREF (pb), NULL);
+	bonobo_object_release_unref (BONOBO_OBJREF (pb), NULL);
 }
 
 static void
@@ -198,6 +145,8 @@ totem_properties_page_finalize (GObject *object)
 
 	g_free(self->location);
 	self->location = NULL;
+	g_object_unref (G_OBJECT (self->xml));
+	self->xml = NULL;
 
 	parent_class->finalize(object);
 }
