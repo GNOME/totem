28afeb8cd323682f348e136e3090d8e8f16322ff
Bastien Nocera <hadess@hadess.net>
2003-08-03 13:26:46 +0000

    change the headers to reflect the LGPL copyright
    2003-08-03  Bastien Nocera  <hadess@hadess.net>
    
    	* src/bacon-resize.c:
    	* src/bacon-resize.h: change the headers to reflect the LGPL
    	copyright

--- src/bacon-resize.c
+++ src/bacon-resize.c
@@ -1,12 +1,21 @@
-/*
-**  Sinek (Media Player)
-**  Copyright (c) 2001-2002 Gurer Ozen
-**
-**  This code is free software; you can redistribute it and/or
-**  modify it under the terms of the GNU General Public License.
-**
-**  screen saver control
-*/
+/* bacon-resize.c
+ * Copyright (C) 2003, Bastien Nocera <hadess@hadess.net>
+ * All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Cambridge, MA 02139, USA.
+ */
 
 #include "config.h"
 #include "scrsaver.h"
