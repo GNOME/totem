1cb72d98ec07a6ba8333fbd68916d9f386db6761
Bastien Nocera <hadess@hadess.net>
2004-04-19 22:48:06 +0000

    Fix the GTK+ only build, disable vanity building by default (we need to
    2004-04-19  Bastien Nocera  <hadess@hadess.net>
    
    	* configure.in:
    	* data/Makefile.am:
    	* data/mozilla-viewer.glade:
    	* src/Makefile.am:
    	* src/totem-mozilla-plugin.c: (totem_plugin_fork),
    	(totem_plugin_new_instance), (totem_plugin_destroy_instance),
    	(totem_plugin_set_window), (totem_plugin_new_stream),
    	(totem_plugin_destroy_stream), (totem_plugin_write_ready),
    	(totem_plugin_write), (totem_plugin_stream_as_file),
    	(totem_plugin_url_notify), (totem_plugin_get_value),
    	(totem_plugin_set_value), (NP_GetValue), (NP_GetMIMEDescription),
    	(NP_Initialize), (NP_Shutdown):
    	* src/totem-mozilla-viewer.c: (totem_embedded_error_and_exit),
    	(totem_embedded_open), (totem_embedded_add_children), (embedded),
    	(main): Fix the GTK+ only build, disable vanity building by default
    	(we need to finish it first...), commit the first version (slightly
    	broken *cough*understatement*cough*) of the Mozilla plugin

---
+++ src/totem-mozilla-plugin.cpp
@@ -0,0 +1,492 @@
+/* Totem Mozilla plugin
+ * 
+ * Copyright (C) <2004> Bastien Nocera <hadess@hadess.net>
+ * Copyright (C) <2002> David A. Schleef <ds@schleef.org>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Library General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Library General Public License for more details.
+ *
+ * You should have received a copy of the GNU Library General Public
+ * License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+ * Boston, MA 02111-1307, USA.
+ */
+
+#include "config.h"
+
+#include <stdio.h>
+#include <stdint.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <string.h>
+
+#include <glib.h>
+
+#ifdef ENABLE_NLS
+#include <libintl.h>
+#define _(String) dgettext(GETTEXT_PACKAGE,String)
+#ifdef gettext_noop
+#define N_(String) gettext_noop(String)
+#else
+#define N_(String) (String)
+#endif /* gettext_noop */
+#else
+#define _(String) (String)
+#define N_(String) (String)
+#endif /* ENABLE_NLS */
+
+#define XP_UNIX 1
+#define MOZ_X11 1
+#include "npapi.h"
+#include "npupp.h"
+
+#define DEBUG(x) printf(x "\n")
+//#define DEBUG(x)
+
+typedef struct {
+	NPP instance;
+	guint32 window;
+
+	int width, height;
+	int recv_fd, send_fd;
+	int player_pid;
+
+	GByteArray *bytes;
+} TotemPlugin;
+
+static NPNetscapeFuncs mozilla_functions;
+
+/* You don't update, you die! */
+#define MAX_ARGV_LEN 10
+
+static void totem_plugin_fork (TotemPlugin *plugin)
+{
+	char **argv;
+	int argc = 0;
+	GError *err = NULL;
+
+	argv = (char **)g_new0 (char *, MAX_ARGV_LEN);
+
+	if (g_file_test ("./totem-mozilla-viewer",
+				G_FILE_TEST_EXISTS) != FALSE) {
+		argv[argc++] = g_strdup ("./totem-mozilla-viewer");
+	} else {
+		argv[argc++] = g_strdup (LIBDIR"/totem-mozilla-viewer");
+	}
+
+	argv[argc++] = "--xid";
+	argv[argc++] = g_strdup_printf ("%d", plugin->window);
+
+	if (plugin->width) {
+		argv[argc++] = g_strdup ("--width");
+		argv[argc++] = g_strdup_printf ("%d", plugin->width);
+	}
+
+	if (plugin->height) {
+		argv[argc++] = g_strdup ("--height");
+		argv[argc++] = g_strdup_printf ("%d", plugin->height);
+	}
+
+	argv[argc++] = g_strdup ("fd://0");
+	argv[argc] = NULL;
+
+	{
+		int i;
+		g_print ("Launching: ");
+		for (i = 0; i < argc; i++) {
+			g_print ("%s ", argv[i]);
+		}
+		g_print ("\n");
+	}
+
+	if (g_spawn_async_with_pipes (NULL, argv, NULL,
+				G_SPAWN_DO_NOT_REAP_CHILD,
+//				| G_SPAWN_STDOUT_TO_DEV_NULL,
+				NULL, NULL, &plugin->player_pid,
+				&plugin->send_fd, NULL, NULL, &err) == FALSE)
+	{
+		DEBUG("Spawn failed");
+
+		if(err)
+		{
+			fprintf(stderr, "%s\n", err->message);
+			g_error_free(err);
+		}
+	}
+
+	g_strfreev (argv);
+}
+
+static NPError totem_plugin_new_instance (NPMIMEType mime_type, NPP instance,
+		uint16_t mode, int16_t argc, char *argn[], char *argv[],
+		NPSavedData *saved)
+{
+	TotemPlugin *plugin;
+	int i;
+
+	DEBUG("totem_plugin_new_instance");
+
+	if (instance == NULL)
+		return NPERR_INVALID_INSTANCE_ERROR;
+
+	instance->pdata = mozilla_functions.memalloc(sizeof(TotemPlugin));
+	plugin = (TotemPlugin *) instance->pdata;
+
+	if (plugin == NULL)
+		return NPERR_OUT_OF_MEMORY_ERROR;
+
+	memset(plugin, 0, sizeof(TotemPlugin));
+
+	/* mode is NP_EMBED, NP_FULL, or NP_BACKGROUND (see npapi.h) */
+	printf("mode %d\n",mode);
+	//printf("mime type: %s\n",pluginType);
+	plugin->instance = instance;
+
+	for (i=0; i<argc; i++) {
+		printf ("argv[%d] %s %s\n", i, argn[i], argv[i]);
+		if (strcmp (argn[i],"width") == 0) {
+			plugin->width = strtol (argv[i], NULL, 0);
+		}
+		if (strcmp (argn[i], "height") == 0) {
+			plugin->height = strtol (argv[i], NULL, 0);
+		}
+
+		//Handle loop
+	}
+
+	//totem_plugin_fork(plugin, 0x32);
+
+	return NPERR_NO_ERROR;
+}
+
+static NPError totem_plugin_destroy_instance (NPP instance, NPSavedData **save)
+{
+	TotemPlugin * plugin;
+
+	DEBUG("plugin_destroy");
+
+	if (instance == NULL)
+		return NPERR_INVALID_INSTANCE_ERROR;
+
+	plugin = (TotemPlugin *) instance->pdata;
+	if (plugin == NULL)
+		return NPERR_NO_ERROR;
+
+//	close(plugin->send_fd);
+//	close(plugin->recv_fd);
+
+//	kill (plugin->player_pid, SIGKILL);
+//	waitpid (plugin->player_pid, NULL, 0);
+
+	mozilla_functions.memfree (instance->pdata);
+	instance->pdata = NULL;
+
+	return NPERR_NO_ERROR;
+}
+
+static NPError totem_plugin_set_window (NPP instance, NPWindow* window)
+{
+	TotemPlugin *plugin;
+
+	DEBUG("plugin_set_window");
+
+	if (instance == NULL)
+		return NPERR_INVALID_INSTANCE_ERROR;
+
+	plugin = (TotemPlugin *) instance->pdata;
+	if (plugin == NULL)
+		return NPERR_INVALID_INSTANCE_ERROR;
+
+	if (plugin->window) {
+		DEBUG ("existing window");
+		if (plugin->window == (Window) window->window) {
+			DEBUG("resize");
+			/* Resize event */
+			/* Not currently handled */
+		} else {
+			DEBUG("change");
+			printf ("ack.  window changed!\n");
+		}
+	} else {
+		NPSetWindowCallbackStruct *ws_info;
+
+		DEBUG("about to fork");
+
+		ws_info = window->ws_info;
+		plugin->window = (Window) window->window;
+
+		totem_plugin_fork (plugin);
+
+		//fcntl(plugin->send_fd, F_SETFL, O_NONBLOCK);
+	}
+
+	DEBUG("leaving plugin_set_window");
+
+	return NPERR_NO_ERROR;
+}
+
+static NPError totem_plugin_new_stream (NPP instance, NPMIMEType type,
+		NPStream* stream_ptr, NPBool seekable, uint16* stype)
+{
+	DEBUG("plugin_new_stream");
+
+	if (instance == NULL)
+		return NPERR_INVALID_INSTANCE_ERROR;
+
+	return NPERR_NO_ERROR;
+}
+
+static NPError totem_plugin_destroy_stream (NPP instance, NPStream* stream,
+		NPError reason)
+{
+	DEBUG("plugin_destroy_stream");
+
+	return NPERR_NO_ERROR;
+}
+
+static int32 totem_plugin_write_ready (NPP instance, NPStream *stream)
+{
+	TotemPlugin *plugin;
+
+	DEBUG("plugin_write_ready");
+
+	if (instance == NULL) {
+		DEBUG("plugin_write_ready instance is NULL");
+		return 0;
+	}
+
+	plugin = (TotemPlugin *) instance->pdata;
+
+	return (8*1024);
+//	return 4096;
+}
+
+static int32 totem_plugin_write (NPP instance, NPStream *stream, int32 offset,
+	int32 len, void *buffer)
+{
+	TotemPlugin *plugin;
+	int ret;
+
+	DEBUG("plugin_write");
+
+	if (instance == NULL)
+		return 0;
+
+	plugin = (TotemPlugin *) instance->pdata;
+
+	if (plugin == NULL)
+		return 0;
+
+	if (!plugin->player_pid)
+		return 0;
+
+	ret = write (plugin->send_fd, buffer, len);
+	if (ret < 0)
+		ret = 0;
+
+	return ret;
+}
+
+static void totem_plugin_stream_as_file (NPP instance, NPStream *stream,
+	const char* fname)
+{
+	TotemPlugin *plugin;
+
+	DEBUG("plugin_stream_as_file");
+
+	if (instance == NULL)
+		return;
+	plugin = (TotemPlugin *) instance->pdata;
+
+	if (plugin == NULL)
+		return;
+
+	printf("plugin_stream_as_file\n");
+}
+
+static void
+totem_plugin_url_notify (NPP instance, const char* url,
+		NPReason reason, void* notifyData)
+{
+	DEBUG("plugin_url_notify");
+}
+
+static NPError
+totem_plugin_get_value (NPP instance, NPPVariable variable,
+		                        void *value)
+{
+	NPError err = NPERR_NO_ERROR;
+
+	DEBUG("plugin_get_value");
+
+	switch (variable) {
+	case NPPVpluginNameString:
+		*((char **)value) = "Totem Mozilla Plugin";
+		break;
+	case NPPVpluginDescriptionString:
+		*((char **)value) =
+			"The <a href=\"http://hadess.net/totem.php3\">Totem</a> plugin handles video and audio streams.";
+		break;
+	case NPPVpluginNeedsXEmbed:
+		*((PRBool *)value) = PR_TRUE;
+		break;
+	default:
+		g_message ("unhandled variable %d", variable);
+		err = NPERR_INVALID_PARAM;
+	}
+
+	return err;
+}
+
+static NPError
+totem_plugin_set_value (NPP instance, NPNVariable variable,
+		                        void *value)
+{
+	DEBUG("plugin_set_value");
+
+	return NPERR_NO_ERROR;
+}
+
+NPError
+NP_GetValue(void *future, NPPVariable variable, void *value)
+{
+	return totem_plugin_get_value (NULL, variable, value);
+}
+
+char *NP_GetMIMEDescription(void)
+{
+	//FIXME i18n
+
+	return
+		/* http://www.apple.com/trailers/ */
+		"video/quicktime:mov:QuickTime video;"
+		/* http://www.nbc.com/nbc/Late_Night_with_Conan_O'Brien/video/triumph.shtml */
+		"application/x-mplayer2::Window Media Plugin;";
+/*
+    		"application/mpeg:mpg:MPEG video;"
+    		"audio/x-pn-windows-acm:wav:WAV audio;"
+    		"audio/x-wav:wav:WAV audio;"
+    		"audio/x-ogg:ogg:Ogg Vorbis audio;"
+    		"audio/basic:au:basic audio;"
+    		"audio/mpeg:mp1:MPEG audio;"
+    		"audio/mpeg:mp2:MPEG audio;"
+    		"audio/mpeg:mp3:MPEG audio;"
+		"video/mpeg:mpg:MPEG video;"
+		"video/mpeg:vob:MPEG video;"
+		"video/quicktime:mov:QuickTime video;"
+		"video/x-msvideo:avi:AVI video"; */
+}
+
+NPError NP_Initialize (NPNetscapeFuncs * moz_funcs,
+		NPPluginFuncs * plugin_funcs)
+{
+	NPError err = NPERR_NO_ERROR;
+	PRBool supportsXEmbed = PR_FALSE;
+	NPNToolkitType toolkit = 0;
+
+	printf ("NP_Initialize\n");
+
+	/* Do we support XEMBED? */
+	err = CallNPN_GetValueProc (moz_funcs->getvalue, NULL,
+			NPNVSupportsXEmbedBool,
+			(void *)&supportsXEmbed);
+
+	if (err != NPERR_NO_ERROR || supportsXEmbed != PR_TRUE)
+		return NPERR_INCOMPATIBLE_VERSION_ERROR;
+
+	/* Are we using a GTK+ 2.x Moz? */
+	err = CallNPN_GetValueProc (moz_funcs->getvalue, NULL,
+			NPNVToolkit, (void *)&toolkit);
+
+	if (err != NPERR_NO_ERROR || toolkit != NPNVGtk2)
+		return NPERR_INCOMPATIBLE_VERSION_ERROR;
+
+	if(moz_funcs == NULL || plugin_funcs == NULL)
+		return NPERR_INVALID_FUNCTABLE_ERROR;
+
+	if ((moz_funcs->version >> 8) > NP_VERSION_MAJOR)
+		return NPERR_INCOMPATIBLE_VERSION_ERROR;
+	if (moz_funcs->size < sizeof (NPNetscapeFuncs))
+		return NPERR_INVALID_FUNCTABLE_ERROR;
+	if (plugin_funcs->size < sizeof (NPPluginFuncs))
+		return NPERR_INVALID_FUNCTABLE_ERROR;
+
+	/*
+	 * Copy all of the fields of the Mozilla function table into our
+	 * copy so we can call back into Mozilla later.  Note that we need
+	 * to copy the fields one by one, rather than assigning the whole
+	 * structure, because the Mozilla function table could actually be
+	 * bigger than what we expect.
+	 */
+	mozilla_functions.size             = moz_funcs->size;
+	mozilla_functions.version          = moz_funcs->version;
+	mozilla_functions.geturl           = moz_funcs->geturl;
+	mozilla_functions.posturl          = moz_funcs->posturl;
+	mozilla_functions.requestread      = moz_funcs->requestread;
+	mozilla_functions.newstream        = moz_funcs->newstream;
+	mozilla_functions.write            = moz_funcs->write;
+	mozilla_functions.destroystream    = moz_funcs->destroystream;
+	mozilla_functions.status           = moz_funcs->status;
+	mozilla_functions.uagent           = moz_funcs->uagent;
+	mozilla_functions.memalloc         = moz_funcs->memalloc;
+	mozilla_functions.memfree          = moz_funcs->memfree;
+	mozilla_functions.memflush         = moz_funcs->memflush;
+	mozilla_functions.reloadplugins    = moz_funcs->reloadplugins;
+	mozilla_functions.getJavaEnv       = moz_funcs->getJavaEnv;
+	mozilla_functions.getJavaPeer      = moz_funcs->getJavaPeer;
+	mozilla_functions.geturlnotify     = moz_funcs->geturlnotify;
+	mozilla_functions.posturlnotify    = moz_funcs->posturlnotify;
+	mozilla_functions.getvalue         = moz_funcs->getvalue;
+	mozilla_functions.setvalue         = moz_funcs->setvalue;
+	mozilla_functions.invalidaterect   = moz_funcs->invalidaterect;
+	mozilla_functions.invalidateregion = moz_funcs->invalidateregion;
+	mozilla_functions.forceredraw      = moz_funcs->forceredraw;
+
+	/*
+	 * Set up a plugin function table that Mozilla will use to call
+	 * into us.  Mozilla needs to know about our version and size and
+	 * have a UniversalProcPointer for every function we implement.
+	 */
+
+	plugin_funcs->size = sizeof(NPPluginFuncs);
+	plugin_funcs->version = (NP_VERSION_MAJOR << 8) + NP_VERSION_MINOR;
+	plugin_funcs->newp = NewNPP_NewProc(totem_plugin_new_instance);
+	plugin_funcs->destroy =
+		NewNPP_DestroyProc(totem_plugin_destroy_instance);
+	plugin_funcs->setwindow =
+		NewNPP_SetWindowProc(totem_plugin_set_window);
+	plugin_funcs->newstream =
+		NewNPP_NewStreamProc(totem_plugin_new_stream);
+	plugin_funcs->destroystream =
+		NewNPP_DestroyStreamProc(totem_plugin_destroy_stream);
+	plugin_funcs->asfile =
+		NewNPP_StreamAsFileProc(totem_plugin_stream_as_file);
+	plugin_funcs->writeready =
+		NewNPP_WriteReadyProc(totem_plugin_write_ready);
+	plugin_funcs->write = NewNPP_WriteProc(totem_plugin_write);
+	/* Printing ? */
+	plugin_funcs->print = NewNPP_PrintProc(NULL);
+	/* What's that for ? */
+	plugin_funcs->event = NewNPP_HandleEventProc(NULL);
+	plugin_funcs->urlnotify =
+		NewNPP_URLNotifyProc(totem_plugin_url_notify);
+	plugin_funcs->javaClass = NULL;
+	plugin_funcs->getvalue = NewNPP_GetValueProc(totem_plugin_get_value);
+	plugin_funcs->setvalue = NewNPP_SetValueProc(totem_plugin_set_value);
+
+	return NPERR_NO_ERROR;
+}
+
+NPError NP_Shutdown(void)
+{
+	return NPERR_NO_ERROR;
+}
+
---
+++ src/totem-mozilla-viewer.c
@@ -0,0 +1,222 @@
+
+#include <gtk/gtk.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <gdk/gdkx.h>
+#include <glade/glade.h>
+
+#include "bacon-video-widget.h"
+
+#ifdef ENABLE_NLS
+#include <libintl.h>
+#define _(String) dgettext(GETTEXT_PACKAGE,String)
+#ifdef gettext_noop
+#define N_(String) gettext_noop(String)
+#else
+#define N_(String) (String)
+#endif /* gettext_noop */
+#else
+#define _(String) (String)
+#define N_(String) (String)
+#endif /* ENABLE_NLS */
+
+
+typedef struct TotemEmbedded TotemEmbedded;
+
+struct TotemEmbedded {
+	GtkWidget *window;
+	GladeXML *xml;
+	int xid, width, height;
+	gboolean use_xembed;
+	char *filename;
+	BaconVideoWidget *bvw;
+	gboolean embedded_done;
+};
+
+static void
+totem_embedded_error_and_exit (char *title, char *reason, TotemEmbedded *emb)
+{
+	GtkWidget *error_dialog;
+
+	error_dialog =
+		gtk_message_dialog_new (NULL,
+				GTK_DIALOG_MODAL,
+				GTK_MESSAGE_ERROR,
+				GTK_BUTTONS_OK,
+				"<b>%s</b>\n%s", title, reason);
+	gtk_container_set_border_width (GTK_CONTAINER (error_dialog), 5);
+	gtk_label_set_use_markup (GTK_LABEL (GTK_MESSAGE_DIALOG (error_dialog)->label), TRUE);
+	gtk_dialog_set_default_response (GTK_DIALOG (error_dialog),
+			GTK_RESPONSE_OK);
+	gtk_window_set_modal (GTK_WINDOW (error_dialog), TRUE);
+	gtk_dialog_run (GTK_DIALOG (error_dialog));
+
+	exit (1);
+}
+
+static void
+totem_embedded_open (TotemEmbedded *emb)
+{
+	GError *err = NULL;
+	gboolean retval;
+	gboolean caps;
+
+	retval = bacon_video_widget_open (emb->bvw, emb->filename, &err);
+	if (retval == FALSE)
+	{
+		char *msg, *disp;
+
+		disp = g_strdup (emb->filename);
+		//disp = gnome_vfs_unescape_string_for_display (totem->mrl);
+		msg = g_strdup_printf(_("Totem could not play '%s'."), disp);
+		g_free (disp);
+		g_message ("error: %s", err->message);
+		//totem_action_error (msg, err->message, totem);
+		g_free (msg);
+
+		g_error_free (err);
+		retval = FALSE;
+	}
+
+	return;
+//	return retval;
+}
+
+static void
+totem_embedded_add_children (TotemEmbedded *emb)
+{
+	GtkWidget *child, *container;
+	char *filename;
+	GError *err = NULL;
+
+	filename = g_build_filename (DATADIR,
+			"totem", "mozilla-viewer.glade", NULL);
+	if (g_file_test (filename, G_FILE_TEST_EXISTS) == FALSE)
+	{
+		g_free (filename);
+		filename = g_build_filename ("..", "data", "mozilla-viewer.glade", NULL);
+	}
+
+	if (g_file_test (filename, G_FILE_TEST_EXISTS) == FALSE)
+	{
+		g_free (filename);
+		totem_embedded_error_and_exit (_("Couldn't load the main interface (mozilla-viewer.glade)."), _("Make sure that the Totem plugin is properly installed."), NULL);
+	}
+
+	emb->xml = glade_xml_new (filename, "vbox1", NULL);
+	if (emb->xml == NULL)
+	{
+		g_free (filename);
+		totem_embedded_error_and_exit (_("Couldn't load the main interface (mozilla-viewer.glade)."), _("Make sure that the Totem plugin is properly installed."), NULL);
+	}
+
+	child = glade_xml_get_widget (emb->xml, "vbox1");
+	gtk_container_add (GTK_CONTAINER (emb->window), child);
+
+	emb->bvw = BACON_VIDEO_WIDGET (bacon_video_widget_new
+			(-1, -1, FALSE, &err));
+
+	if (emb->bvw == NULL)
+	{
+		totem_embedded_error_and_exit (_("The Totem plugin could not startup."), err != NULL ? err->message : _("No reason."), emb);
+		if (err != NULL)
+			g_error_free (err);
+	}
+
+	container = glade_xml_get_widget (emb->xml, "hbox4");
+	gtk_container_add (GTK_CONTAINER (container), GTK_WIDGET (emb->bvw));
+	gtk_widget_show (GTK_WIDGET (emb->bvw));
+
+	gtk_widget_realize (emb->window);
+	gtk_widget_set_size_request (emb->window, emb->width, emb->height);
+}
+
+static void embedded (GtkPlug *plug, TotemEmbedded *emb)
+{
+	printf("EMBEDDED!\n");
+	emb->embedded_done = TRUE;
+//	gtk_widget_show_all((GtkWidget *)data);
+}
+
+int main (int argc, char **argv)
+{
+	TotemEmbedded *emb;
+	int i;
+
+	emb = g_new0 (TotemEmbedded, 1);
+	emb->use_xembed = TRUE;
+	emb->width = emb->height = -1;
+
+	if (XInitThreads () == 0)
+	{
+		gtk_init (&argc, &argv);
+		totem_embedded_error_and_exit (_("Could not initialise the thread-safe libraries."), _("Verify your system installation. The Totem plugin will now exit."), NULL);
+	}
+
+	g_thread_init (NULL);
+	gdk_threads_init ();
+
+	gtk_init (&argc, &argv);
+
+	g_print ("CMD line: ");
+	for (i = 1; i < argc; i++) {
+		g_print ("%s ", argv[i]);
+	}
+	g_print ("\n");
+
+	/* TODO Add popt options */
+	for (i = 1; i < argc; i++) {
+		if (strcmp (argv[i], "--xid") == 0) {
+			if (i + 1 < argc) {
+				i++;
+				emb->xid = atoi (argv[i]);
+			}
+		} else if (strcmp (argv[i], "--width") == 0) {
+			if (i + 1 < argc) {
+				i++;
+				emb->width = atoi (argv[i]);
+			}
+		} else if (strcmp (argv[i], "--height") == 0) {
+			if (i + 1 < argc) {
+				i++;
+				emb->height = atoi (argv[i]);
+			}
+		} else if (i + 1 == argc) {
+			emb->filename = argv[i];
+		}
+	}
+
+	/* XEMBED or stand-alone */
+	if (emb->xid != 0) {
+		GtkWidget *window;
+
+		/* The miraculous XEMBED protocol */
+		window = gtk_plug_new ((GdkNativeWindow)emb->xid);
+		gtk_signal_connect(GTK_OBJECT(window), "embedded",
+				G_CALLBACK (embedded), NULL);
+		gtk_widget_realize (window);
+
+		emb->window = window;
+	} else {
+		/* Stand-alone version */
+		emb->window = gtk_window_new (GTK_WINDOW_TOPLEVEL);
+	}
+
+	totem_embedded_add_children (emb);
+
+	gtk_widget_show (emb->window);
+
+	/* wait until we're embedded if we're to be */
+	if (emb->xid != 0)
+		while (emb->embedded_done == FALSE && gtk_events_pending ())
+			gtk_main_iteration ();
+
+//	totem_embedded_open (emb);
+//	bacon_video_widget_play (emb->bvw, NULL);
+
+	gtk_main ();
+
+	return 0;
+}
+
