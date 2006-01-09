300cf2ec3e4f3debfdb2941b0dd0895fe656ed5e
Bastien Nocera <hadess@hadess.net>
2006-01-09 13:09:05 +0000

    make the Totem plugin resident in memory to avoid re-registering its type
    2006-01-09  Bastien Nocera  <hadess@hadess.net>
    
    	* src/totem-mozilla-plugin.cpp: make the Totem plugin resident in
    	memory to avoid re-registering its type via dbus-glib, fixes
    	crashes when viewing some videos a second time, with help from
    	Christian Persch <chpe@cvs.gnome.org> (Closes: #324352)

--- src/totem-mozilla-plugin.cpp
+++ src/totem-mozilla-plugin.cpp
@@ -227,6 +227,10 @@ static NPError totem_plugin_new_instance (NPMIMEType mime_type, NPP instance,
 	if (instance == NULL)
 		return NPERR_INVALID_INSTANCE_ERROR;
 
+	/* Make sure the plugin stays resident to avoid crashers when 
+	 * reloading the GObject types */
+	NPN_SetValue (instance, NPPVpluginKeepLibraryInMemory, (void *)TRUE);
+
 	instance->pdata = mozilla_functions.memalloc(sizeof(TotemPlugin));
 	plugin = (TotemPlugin *) instance->pdata;
 
