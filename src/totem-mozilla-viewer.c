c144456e9840bed9ed34163d15c5dc19b0e2cda5
Bastien Nocera <hadess@hadess.net>
2006-07-03 21:29:37 +0000

    add i18n support (Closes: #346492)
    2006-07-03  Bastien Nocera  <hadess@hadess.net>
    
    	* src/totem-mozilla-viewer.c: (main): add i18n support
    	(Closes: #346492)
    
    2006-07-03  Bastien Nocera  <hadess@hadess.net>
    
    	* POTFILES.in:
    	* POTFILES.skip: do translate src/totem-mozilla-viewer.c

--- src/totem-mozilla-viewer.c
+++ src/totem-mozilla-viewer.c
@@ -644,6 +644,10 @@ int main (int argc, char **argv)
 	gchar *svcname;
 	GError *e = NULL;
 
+	bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
+	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+	textdomain (GETTEXT_PACKAGE);
+
 	if (XInitThreads () == 0)
 	{
 		gtk_init (&argc, &argv);
