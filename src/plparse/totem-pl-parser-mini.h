4aa2c72eb79bb28957c1a230ecc1f48055cd1a70
Bastien Nocera <hadess@hadess.net>
2006-08-11 10:54:23 +0000

    add file missing from previous commit
    2006-08-11  Bastien Nocera  <hadess@hadess.net>
    
    	* src/plparse/totem-pl-parser-mini.h: add file missing from
    	previous commit

---
+++ src/plparse/totem-pl-parser-mini.h
@@ -0,0 +1,36 @@
+/* 
+   Copyright (C) 2002, 2003, 2004, 2005, 2006 Bastien Nocera
+   Copyright (C) 2003 Colin Walters <walters@verbum.org>
+
+   The Gnome Library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public License as
+   published by the Free Software Foundation; either version 2 of the
+   License, or (at your option) any later version.
+
+   The Gnome Library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public
+   License along with the Gnome Library; see the file COPYING.LIB.  If not,
+   write to the Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+   Boston, MA 02111-1307, USA.
+
+   Author: Bastien Nocera <hadess@hadess.net>
+ */
+
+#ifndef TOTEM_PL_PARSER_MINI_H
+#define TOTEM_PL_PARSER_MINI_H
+
+#include <glib.h>
+
+G_BEGIN_DECLS
+
+gboolean totem_pl_parser_can_parse_from_data	(const char *data,
+ 						 gsize len,
+						 gboolean debug);
+
+G_END_DECLS
+
+#endif /* TOTEM_PL_PARSER_MINI_H */
