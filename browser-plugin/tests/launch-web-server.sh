b9f69e34ee4bf87a89ae926653fbce794f7ad8bd
Bastien Nocera <hadess@hadess.net>
2006-11-14 08:29:41 +0000

    add a script to startup Apache for plugin testing purposes
    2006-11-14  Bastien Nocera  <hadess@hadess.net>
    
    	* browser-plugin/tests/.cvsignore:
    	* browser-plugin/tests/.htaccess:
    	* browser-plugin/tests/index.html:
    	* browser-plugin/tests/launch-web-server.sh: add a script to startup
    	Apache for plugin testing purposes

---
+++ browser-plugin/tests/launch-web-server.sh
@@ -0,0 +1,94 @@
+#!/bin/sh
+
+# Port to listen on
+PORT=12345
+
+# Find Apache first
+
+for i in /usr/sbin/httpd ; do
+	if [ -x $i ] ; then
+		HTTPD=$i
+	fi
+done
+
+if [ -z $HTTPD ] ; then
+	echo "Could not find httpd at the usual locations"
+	exit 1
+fi
+
+# Check whether we in the right directory
+
+if [ ! -f `basename $0` ] ; then
+	echo "You need to launch `basename $0` from within its directory"
+	echo "eg: ./`basename $0` [stop | start]"
+	exit 1
+fi
+
+ROOTDIR=`dirname $0`/root
+
+# See if we shoud stop the web server
+
+if [ -z $1 ] ; then
+	echo "Usage: ./`basename $0` [stop | start]"
+	exit 1
+fi
+
+if [ $1 = stop ] ; then
+	echo "Trying to stop $HTTPD(`cat root/pid`)"
+	pushd root/ > /dev/null
+	$HTTPD -f `pwd`/conf -k stop
+	popd > /dev/null
+	exit
+fi
+
+# Setup the ServerRoot
+
+if [ ! -d $ROOTDIR ] ; then
+	mkdir -p root/ || ( echo "Could not create the ServerRoot" ; exit 1 )
+fi
+
+DOCDIR=`pwd`
+pushd root/ > /dev/null
+# Resolve the relative ROOTDIR path
+ROOTDIR=`pwd`
+if [ -f pid ] && [ -f conf ] ; then
+	$HTTPD -f $ROOTDIR/conf -k stop
+	sleep 2
+fi
+rm -f conf pid lock log
+
+# Setup the config file
+
+echo "ServerRoot \"$ROOTDIR\""                  >> conf
+echo "PidFile pid"                              >> conf
+echo "LockFile lock"                            >> conf
+#echo "LogLevel crit"                            >> conf
+echo "LogLevel info"                            >> conf
+echo "ErrorLog log"                             >> conf
+echo                                            >> conf
+echo                                            >> conf
+echo "LoadModule env_module /etc/httpd/modules/mod_env.so" >> conf
+echo "LoadModule mime_magic_module /etc/httpd/modules/mod_mime_magic.so" >> conf
+echo "LoadModule mime_module /etc/httpd/modules/mod_mime.so" >> conf
+echo "LoadModule dir_module /etc/httpd/modules/mod_dir.so" >> conf
+echo "LoadModule autoindex_module /etc/httpd/modules/mod_autoindex.so" >> conf
+echo                                            >> conf
+echo "TypesConfig /etc/mime.types"              >> conf
+echo "DocumentRoot \"$DOCDIR\""                 >> conf
+echo "<Directory \"$DOCDIR\">"                  >> conf
+echo "AllowOverride All"                        >> conf
+echo "</Directory>"                             >> conf
+echo                                            >> conf
+echo "StartServers 1"                           >> conf
+echo "MinSpareServers 1"                        >> conf
+echo "MaxSpareServers 1"                        >> conf
+echo "MaxClients 3"                             >> conf
+echo                                            >> conf
+
+popd > /dev/null
+
+# Launch!
+
+$HTTPD -f $ROOTDIR/conf -C "Listen 127.0.0.1:$PORT"
+echo "Please start debugging at http://localhost:$PORT/"
+
