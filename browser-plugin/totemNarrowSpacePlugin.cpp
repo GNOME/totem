821d85464840f659f6e34088b84c4f61f0cfb87d
Bastien Nocera <hadess@hadess.net>
2007-01-11 23:54:56 +0000

    support application/x-quicktime-media-link in the QuickTime plugin to
    2007-01-11  Bastien Nocera  <hadess@hadess.net>
    
    	* browser-plugin/totemNarrowSpacePlugin.cpp: support
    	application/x-quicktime-media-link in the QuickTime plugin to
    	allow playback at
    	http://aftonbladet.se/special/webbtv/rockbjornen/quicktime.html
    	(Closes: #395564)
    
    
    svn path=/trunk/; revision=3863

--- browser-plugin/totemNarrowSpacePlugin.cpp
+++ browser-plugin/totemNarrowSpacePlugin.cpp
@@ -50,7 +50,8 @@ static const totemPluginMimeEntry kMimeTypes[] = {
 	{ "video/quicktime", "mov", NULL, FALSE },
 	{ "video/mp4", "mp4", NULL, FALSE },
 	{ "image/x-macpaint", "pntg", NULL, FALSE },
-	{ "image/x-quicktime", "pict, pict1, pict2", "image/x-pict", FALSE }
+	{ "image/x-quicktime", "pict, pict1, pict2", "image/x-pict", FALSE },
+	{ "application/x-quicktime-media-link", "qtl", NULL, TRUE }
 };
 
 totemScriptablePlugin::totemScriptablePlugin (totemPlugin *aPlugin)
