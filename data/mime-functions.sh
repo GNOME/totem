a12b370e6b4cd5c6e9d4bdc07a2284baede2d3fd
Bastien Nocera <hadess@hadess.net>
2007-02-08 16:10:39 +0000

    Patch from Sebastien Bacher <seb128@debian.org> to make the new
    2007-02-08  Bastien Nocera  <hadess@hadess.net>
    
    	* data/mime-functions.sh: Patch from Sebastien Bacher
    	<seb128@debian.org> to make the new mime-functions.sh
    	work with dash (Closes: #405758)
    
    
    svn path=/trunk/; revision=4016

--- data/mime-functions.sh
+++ data/mime-functions.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
-function get_audio_mimetypes ()
+get_audio_mimetypes ()
 {
 	MIMETYPES=`cat $1 | grep "\/" | grep audio | grep -v "audio/x-pn-realaudio"`
 }
 
-function get_video_mimetypes ()
+get_video_mimetypes ()
 {
 	MIMETYPES=`cat $1 | grep -v audio | grep -v "application/x-flac"`
 	MIMETYPES="$MIMETYPES audio/x-pn-realaudio"
