9c30ec13f6b222ae06572dc4cae73cbb7318af6d
Bastien Nocera <hadess@hadess.net>
2007-05-07 16:42:02 +0000

    When the window becomes maximised, disable the fit window to movie menu
    2007-05-07  Bastien Nocera  <hadess@hadess.net>
    
    	* src/totem.c: (window_state_event_cb): When the window becomes
    	maximised, disable the fit window to movie menu entries, as we
    	can't fit to movie when we're maximised (Closes: #129580)
    
    
    svn path=/trunk/; revision=4289

--- src/totem.c
+++ src/totem.c
@@ -610,6 +610,9 @@ window_state_event_cb (GtkWidget *window, GdkEventWindowState *event,
 				& GDK_WINDOW_STATE_MAXIMIZED) != 0;
                 gtk_statusbar_set_has_resize_grip (GTK_STATUSBAR (totem->statusbar),
                                                    !totem->maximised);
+		totem_action_set_sensitivity ("zoom-1-2", !totem->maximised);
+		totem_action_set_sensitivity ("zoom-1-1", !totem->maximised);
+		totem_action_set_sensitivity ("zoom-2-1", !totem->maximised);
 		return FALSE;
 	}
 
