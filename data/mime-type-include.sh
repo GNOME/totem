d2ddc392c7d80bf6dc728e8c7b6385308b34c8fb
Bastien Nocera <hadess@hadess.net>
2007-10-16 14:40:28 +0000

    generate nautilus-audio-mime-types.h for use in nautilus itself
    2007-10-16  Bastien Nocera  <hadess@hadess.net>
    
    	* data/Makefile.am:
    	* data/mime-type-include.sh: generate nautilus-audio-mime-types.h
    	for use in nautilus itself
    
    
    svn path=/trunk/; revision=4778

--- data/mime-type-include.sh
+++ data/mime-type-include.sh
@@ -6,6 +6,22 @@ echo_mime () {
 	echo "\"$i\","
 }
 
+if [ x"$1" == "x--nautilus" ] ; then
+	get_audio_mimetypes $2;
+
+	echo "/* generated with mime-types-include.sh in the totem module, don't edit or "
+	echo "   commit in the nautilus module without filing a bug against totem */"
+
+	echo "static char *audio_mime_types[] = {"
+	for i in $MIMETYPES ; do
+		echo_mime;
+	done
+
+	echo "};"
+
+	exit 0
+fi
+
 MIMETYPES=`cat $1`
 
 echo "/* generated with mime-types-include.sh, don't edit */"
