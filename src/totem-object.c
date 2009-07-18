f53e438e0e530e5cbbd7ed607df498310645013d
Robin Stocker <robin@nibor.org>
2009-07-18 11:15:42 +0200

    Bug 588937 – Drag and drop on itself with Alt appends instead of asking
    2009-07-18  Robin Stocker  <robin@nibor.org>
    
    	* src/totem-object.c (drop_video_cb, drop_playlist_cb):
    	When drag and dropping a video on itself with Alt pressed, ask
    	what to do instead of appending.

--- src/totem-object.c
+++ src/totem-object.c
@@ -2210,19 +2210,19 @@ drop_video_cb (GtkWidget     *widget,
 {
 	gboolean empty_pl;
 
-	if (context->suggested_action == GDK_ACTION_ASK) {
+	/* Drop of video on itself */
+	if (context->source_window == totem->video_drag_source_window &&
+	    context->action == GDK_ACTION_MOVE) {
+		gtk_drag_finish (context, FALSE, FALSE, time);
+		return;
+	}
+
+	if (context->action == GDK_ACTION_ASK) {
 		context->action = totem_drag_ask (totem_get_playlist_length (totem) > 0);
 	}
 
 	/* User selected cancel */
 	if (context->action == GDK_ACTION_DEFAULT) {
-		gtk_drag_finish (context, FALSE, FALSE, time);
-		return;
-	}
-
-	/* Drop of video on itself */
-	if (context->source_window == totem->video_drag_source_window &&
-	    context->action == GDK_ACTION_MOVE) {
 		gtk_drag_finish (context, FALSE, FALSE, time);
 		return;
 	}
@@ -2265,7 +2265,7 @@ drop_playlist_cb (GtkWidget     *widget,
 {
 	gboolean empty_pl;
 
-	if (context->suggested_action == GDK_ACTION_ASK)
+	if (context->action == GDK_ACTION_ASK)
 		context->action = totem_drag_ask (totem_get_playlist_length (totem) > 0);
 
 	if (context->action == GDK_ACTION_DEFAULT) {
