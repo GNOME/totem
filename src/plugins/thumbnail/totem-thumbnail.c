7b87251597b49210ed4778aca6d4ebdf47aa8bed
Bastien Nocera <hadess@hadess.net>
2010-06-16 18:50:56 +0100

    Cleanup thumbnail plugin definition

--- src/plugins/thumbnail/totem-thumbnail.c
+++ src/plugins/thumbnail/totem-thumbnail.c
@@ -36,6 +36,7 @@
 #include <gmodule.h>
 #include <string.h>
 
+#include "totem-plugin.h"
 #include "totem.h"
 
 #define TOTEM_TYPE_THUMBNAIL_PLUGIN		(totem_thumbnail_plugin_get_type ())
@@ -64,19 +65,9 @@ typedef struct
 	PeasExtensionBaseClass parent_class;
 } TotemThumbnailPluginClass;
 
-G_MODULE_EXPORT void peas_register_types	(PeasObjectModule *module);
 GType totem_thumbnail_plugin_get_type		(void) G_GNUC_CONST;
 
-static void peas_activatable_iface_init		(PeasActivatableInterface *iface);
-static void impl_activate			(PeasActivatable *plugin, GObject *object);
-static void impl_deactivate			(PeasActivatable *plugin, GObject *object);
-
-G_DEFINE_DYNAMIC_TYPE_EXTENDED (TotemThumbnailPlugin,
-				totem_thumbnail_plugin,
-				PEAS_TYPE_EXTENSION_BASE,
-				0,
-				G_IMPLEMENT_INTERFACE_DYNAMIC (PEAS_TYPE_ACTIVATABLE,
-							       peas_activatable_iface_init))
+TOTEM_PLUGIN_REGISTER(TOTEM_TYPE_THUMBNAIL_PLUGIN, TotemThumbnailPlugin, totem_thumbnail_plugin)
 
 static void
 totem_thumbnail_plugin_class_init (TotemThumbnailPluginClass *klass)
@@ -90,18 +81,6 @@ totem_thumbnail_plugin_init (TotemThumbnailPlugin *plugin)
 	plugin->priv = G_TYPE_INSTANCE_GET_PRIVATE (plugin,
 						    TOTEM_TYPE_THUMBNAIL_PLUGIN,
 						    TotemThumbnailPluginPrivate);
-}
-
-static void
-peas_activatable_iface_init (PeasActivatableInterface *iface)
-{
-	iface->activate = impl_activate;
-	iface->deactivate = impl_deactivate;
-}
-
-static void
-totem_thumbnail_plugin_class_finalize (TotemThumbnailPluginClass *klass)
-{
 }
 
 static void
@@ -229,12 +208,3 @@ impl_deactivate (PeasActivatable *plugin,
 	set_icon_to_default (totem);
 }
 
-G_MODULE_EXPORT void
-peas_register_types (PeasObjectModule *module)
-{
-	totem_thumbnail_plugin_register_type (G_TYPE_MODULE (module));
-
-	peas_object_module_register_extension_type (module,
-						    PEAS_TYPE_ACTIVATABLE,
-						    TOTEM_TYPE_THUMBNAIL_PLUGIN);
-}
