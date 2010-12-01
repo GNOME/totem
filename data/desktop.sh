96601954b736c31a9daa83acac777ea6946d4182
Bastien Nocera <hadess@hadess.net>
2010-12-01 18:09:24 +0000

    data: Separate the support URI schemes to another file
    So that we can use it in scripts.

--- data/desktop.sh
+++ data/desktop.sh
@@ -4,17 +4,19 @@ echo_mime () {
 	printf "$i;";
 }
 
+echo_handler () {
+	printf "x-scheme-handler/$i;";
+}
+
 MIMETYPES=`grep -v ^# $1`
 printf MimeType=;
 for i in $MIMETYPES ; do
 	echo_mime;
 done
 
-# URI scheme handlers
-SCHEMES="pnm mms net rtp rtsp mmsh uvox icy icyx"
-
-for i in $SCHEMES ; do
-	printf "x-scheme-handler/$i;"
+MIMETYPES=`grep -v ^# $2`
+for i in $MIMETYPES ; do
+	echo_handler;
 done
 
 echo ""
