09a39f6a9fd66dfb9021180974913d00b36769c6
Bastien Nocera <hadess@hadess.net>
2011-01-27 18:17:18 +0000

    backend: Remove unused TOTEM_OBJECT_HAS_SIGNAL macro

--- src/backend/video-utils.h
+++ src/backend/video-utils.h
@@ -28,8 +28,6 @@
 #include <gdk/gdk.h>
 #include <gtk/gtk.h>
 
-#define TOTEM_OBJECT_HAS_SIGNAL(obj, name) (g_signal_lookup (name, g_type_from_name (G_OBJECT_TYPE_NAME (obj))) != 0)
-
 void totem_gdk_window_set_invisible_cursor (GdkWindow *window);
 void totem_gdk_window_set_waiting_cursor (GdkWindow *window);
 
