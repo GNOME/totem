d8704a0ff1cf0af37ebeb686362241b596f512b1
Philip Withnall <philip@tecnocode.co.uk>
2011-04-04 23:59:44 +0100

    plugins: Add an __init__ to the samplepython plugin and spice it up a bit
    Now it irritatingly toggles fullscreen mode when being deactivated, as well
    as when being activated!
    
    Helps: bgo#645739

--- src/plugins/samplepython/samplepython.py
+++ src/plugins/samplepython/samplepython.py
@@ -1,11 +1,14 @@
 # From code by James Livingston
 
-from gi.repository import GObject, Peas, Totem # pylint: disable-msg=E0611
+from gi.repository import GObject, Peas, Totem # pylint: disable-msg=E0611,W0611
 
 class SamplePython (GObject.Object, Peas.Activatable):
     __gtype_name__ = 'SamplePython'
 
     object = GObject.property (type = GObject.Object)
+
+    def __init__ (self):
+        GObject.Object.__init__ (self)
 
     def do_activate (self):
         print "Activating sample Python plugin"
@@ -13,3 +16,4 @@ class SamplePython (GObject.Object, Peas.Activatable):
 
     def do_deactivate (self):
         print "Deactivating sample Python plugin"
+        self.object.action_fullscreen_toggle ()
