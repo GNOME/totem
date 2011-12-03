14abd80b16170aff306b5e32a727cc665f8c7c63
Bastien Nocera <hadess@hadess.net>
2011-12-03 16:02:38 +0100

    browser-plugin: Don't make dbus-glib resident
    As it's not used anymore.

--- browser-plugin/totemPluginGlue.cpp
+++ browser-plugin/totemPluginGlue.cpp
@@ -25,7 +25,6 @@
 #include <string.h>
 
 #include <gio/gio.h>
-#include <dlfcn.h>
 
 #include "npapi.h"
 #include "npupp.h"
@@ -431,23 +430,6 @@ NP_Initialize (NPNetscapeFuncs *aMozillaVTable,
 	if (err != NPERR_NO_ERROR || toolkit != NPNVGtk2)
 		return NPERR_INCOMPATIBLE_VERSION_ERROR;
 #endif
-	/* we want to open libdbus-glib-1.so.2 in such a way
-	 * in such a way that it becomes permanentely resident */
-	void *handle;
-#if defined(__OpenBSD__)
-	handle = dlopen ("libdbus-glib-1.so", RTLD_NOW | RTLD_NODELETE);
-#elif defined(__CYGWIN__)
-	handle = dlopen ("cygdbus-glib-1-2.dll", RTLD_NOW);
-#else
-	handle = dlopen ("libdbus-glib-1.so.2", RTLD_NOW | RTLD_NODELETE);
-#endif
-	if (!handle) {
-		fprintf (stderr, "%s\n", dlerror()); 
-		return NPERR_MODULE_LOAD_FAILED_ERROR;
-	}
-	/* RTLD_NODELETE allows us to close right away ... */
-	dlclose(handle);
-
 	/*
 	 * Set up a plugin function table that Mozilla will use to call
 	 * into us.  Mozilla needs to know about our version and size and
