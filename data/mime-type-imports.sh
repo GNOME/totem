7224032522e95365d57d8940d60d8e22cbe7750f
Cosimo Cecchi <cosimoc@gnome.org>
2012-04-19 17:06:46 -0400

    data: split audio and video mime types into two separate arrays
    This is needed for Sushi, since the two groups are handled by two
    different viewers.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=674439

--- data/mime-type-imports.sh
+++ data/mime-type-imports.sh
@@ -10,11 +10,13 @@ echo_mime () {
 echo "/* generated with mime-type-imports.sh in the totem module, don't edit or"
 echo "   commit in the sushi module without filing a bug against totem */"
 
-echo "let videoTypes = ["
+echo "let audioTypes = ["
 get_audio_mimetypes $1;
 for i in $MIMETYPES ; do
 	echo_mime;
 done
+echo -e "];\n"
+echo "let videoTypes = ["
 get_video_mimetypes $1;
 for i in $MIMETYPES ; do
 	echo_mime;
