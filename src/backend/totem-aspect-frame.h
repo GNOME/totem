ee0cf00527862f4ce5adc557ae453f5cb83a2c59
Bastien Nocera <hadess@hadess.net>
2012-06-27 12:08:24 +0100

    backend: Remove useless class padding

--- src/backend/totem-aspect-frame.h
+++ src/backend/totem-aspect-frame.h
@@ -65,13 +65,6 @@ struct _TotemAspectFrame
 struct _TotemAspectFrameClass
 {
   MxBinClass parent_class;
-
-  /* padding for future expansion */
-  void (*_padding_0) (void);
-  void (*_padding_1) (void);
-  void (*_padding_2) (void);
-  void (*_padding_3) (void);
-  void (*_padding_4) (void);
 };
 
 GType           totem_aspect_frame_get_type    (void) G_GNUC_CONST;
