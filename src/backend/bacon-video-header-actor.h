a5b0bf2a54118b4fd6e3b5af0e5500e3b5ae52f9
Philip Withnall <philip.withnall@collabora.co.uk>
2014-01-10 21:29:04 +0000

    general: Update FSF licence address
    Instead of hard-coding the postal address for the FSF, reference the GNU
    website.
    
    https://bugzilla.gnome.org/show_bug.cgi?id=721520

--- src/backend/bacon-video-header-actor.h
+++ src/backend/bacon-video-header-actor.h
@@ -15,9 +15,7 @@
  * details.
  *
  * You should have received a copy of the GNU Lesser General
- * Public License along with this program; if not, write to the
- * Free Software Foundation, Inc., 59 Temple Place - Suite 330,
- * Boston, MA 02111-1307, USA.
+ * Public License along with this program; if not, see <http://www.gnu.org/licenses/>.
  *
  */
 
--- src/totem-interface.c
+++ src/totem-interface.c
@@ -14,8 +14,7 @@
  *
  *  You should have received a copy of the GNU Library General Public
  *  License along with the Gnome Library; see the file COPYING.LIB.  If not,
- *  write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- *  Boston, MA 02110-1301  USA.
+ *  see <http://www.gnu.org/licenses/>.
  *
  *  Author: Bastien Nocera <hadess@hadess.net>
  * 
