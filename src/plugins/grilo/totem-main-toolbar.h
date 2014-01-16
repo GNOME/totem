8c75bcb7ec8341009c516e00ceee0b31824b1a5e
Bastien Nocera <hadess@hadess.net>
2014-01-16 14:27:48 +0100

    grilo: Coding style fixes

--- src/plugins/grilo/totem-main-toolbar.h
+++ src/plugins/grilo/totem-main-toolbar.h
@@ -56,30 +56,29 @@ struct _TotemMainToolbarClass
   GtkBoxClass parent_class;
 };
 
-GType           totem_main_toolbar_get_type          (void) G_GNUC_CONST;
-GtkWidget*      totem_main_toolbar_new               (void);
-void            totem_main_toolbar_set_search_mode   (TotemMainToolbar *bar,
-						      gboolean        search_mode);
-
-gboolean        totem_main_toolbar_get_search_mode   (TotemMainToolbar *bar);
-void            totem_main_toolbar_set_select_mode   (TotemMainToolbar *bar,
-						      gboolean          select_mode);
-gboolean        totem_main_toolbar_get_select_mode   (TotemMainToolbar *bar);
-void            totem_main_toolbar_set_title         (TotemMainToolbar *bar,
-						      const char       *title);
-const char *    totem_main_toolbar_get_title         (TotemMainToolbar *bar);
-void            totem_main_toolbar_set_subtitle      (TotemMainToolbar *bar,
-						      const char       *subtitle);
-const char *    totem_main_toolbar_get_subtitle      (TotemMainToolbar *bar);
-void            totem_main_toolbar_set_search_string (TotemMainToolbar *bar,
-						      const char       *search_string);
-const char *    totem_main_toolbar_get_search_string (TotemMainToolbar *bar);
-void            totem_main_toolbar_set_n_selected    (TotemMainToolbar *bar,
-						      guint             n_selected);
-guint           totem_main_toolbar_get_n_selected    (TotemMainToolbar *bar);
-void            totem_main_toolbar_set_custom_title  (TotemMainToolbar *bar,
-						      GtkWidget        *title_widget);
-GtkWidget *     totem_main_toolbar_get_custom_title  (TotemMainToolbar *bar);
+GType           totem_main_toolbar_get_type              (void) G_GNUC_CONST;
+GtkWidget*      totem_main_toolbar_new                   (void);
+void            totem_main_toolbar_set_search_mode       (TotemMainToolbar *bar,
+							  gboolean          search_mode);
+gboolean        totem_main_toolbar_get_search_mode       (TotemMainToolbar *bar);
+void            totem_main_toolbar_set_select_mode       (TotemMainToolbar *bar,
+							  gboolean          select_mode);
+gboolean        totem_main_toolbar_get_select_mode       (TotemMainToolbar *bar);
+void            totem_main_toolbar_set_title             (TotemMainToolbar *bar,
+							  const char       *title);
+const char *    totem_main_toolbar_get_title             (TotemMainToolbar *bar);
+void            totem_main_toolbar_set_subtitle          (TotemMainToolbar *bar,
+							  const char       *subtitle);
+const char *    totem_main_toolbar_get_subtitle          (TotemMainToolbar *bar);
+void            totem_main_toolbar_set_search_string     (TotemMainToolbar *bar,
+						          const char       *search_string);
+const char *    totem_main_toolbar_get_search_string     (TotemMainToolbar *bar);
+void            totem_main_toolbar_set_n_selected        (TotemMainToolbar *bar,
+							  guint             n_selected);
+guint           totem_main_toolbar_get_n_selected        (TotemMainToolbar *bar);
+void            totem_main_toolbar_set_custom_title      (TotemMainToolbar *bar,
+							  GtkWidget        *title_widget);
+GtkWidget *     totem_main_toolbar_get_custom_title      (TotemMainToolbar *bar);
 void            totem_main_toolbar_set_select_menu_model (TotemMainToolbar *bar,
 							  GMenuModel       *model);
 GMenuModel *    totem_main_toolbar_get_select_menu_model (TotemMainToolbar *bar);
