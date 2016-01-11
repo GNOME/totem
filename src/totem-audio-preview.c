8ed76341025868862f380f40704b9d4683b1eca0
Ting-Wei Lan <lantw@src.gnome.org>
2016-01-11 14:38:35 +0800

    audio-preview: Fix help messages in some locales
    Call setlocale() first thing in the main() function
    
    https://bugzilla.gnome.org/show_bug.cgi?id=760434

--- src/totem-audio-preview.c
+++ src/totem-audio-preview.c
@@ -163,6 +163,7 @@ int main (int argc, char **argv)
 	GMainLoop *loop;
 	GstElement *play;
 
+	setlocale (LC_ALL, "");
 	bindtextdomain (GETTEXT_PACKAGE, GNOMELOCALEDIR);
 	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
 	textdomain (GETTEXT_PACKAGE);
