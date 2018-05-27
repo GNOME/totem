GNOME/totem

commit c53f91a3b6458bddc9261077fcf49d46f16fa1a6

pythonconsole: Import all classes to expose publicly
The PythonConsole and OutFile classes are exposed publicly by the
console (so that the user can use them dynamically from the console); so
both must be imported from their module first.

https://bugzilla.gnome.org/show_bug.cgi?id=759075

src/plugins/pythonconsole/pythonconsole.py
@@ -32,7 +32,7 @@
 # Monday 7th February 2005: Christian Schaller: Add exception clause.
 # See license_change file for details.
 
-from console import PythonConsole
+from console import PythonConsole, OutFile
 
 __all__ = ('PythonConsole', 'OutFile') # pylint: disable-msg=E0603
 

commit 690439cd2ebffc96f6d9eccd78383e223a348555

Fix the broken python console plugin
In commit c53f91a3b6458bddc9261077fcf49d46f16fa1a6, "__all__" is in the wrong
file pythonconsole.py instead of console.py.

https://gitlab.gnome.org/GNOME/totem/issues/232

Closes: #232

src/plugins/pythonconsole/console.py
@@ -42,6 +42,8 @@ gi.require_version('Pango', '1.0')
 
 from gi.repository import GLib, Pango, Gtk, Gdk # pylint: disable-msg=E0611
 
+__all__ = ('PythonConsole', 'OutFile') # pylint: disable-msg=E0603
+
 class PythonConsole(Gtk.ScrolledWindow): # pylint: disable-msg=R0902
     def __init__(self, namespace = {}, # pylint: disable-msg=W0102
                  destroy_cb = None):

src/plugins/pythonconsole/pythonconsole.py
@@ -44,8 +44,6 @@ from gi.repository import Gio # pylint: disable-msg=E0611
 
 from console import PythonConsole, OutFile
 
-__all__ = ('PythonConsole', 'OutFile') # pylint: disable-msg=E0603
-
 try:
     import rpdb2
     HAVE_RPDB2 = True