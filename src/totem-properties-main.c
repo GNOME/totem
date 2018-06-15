dc55e085f9ecf059f9a6dcdaef169a13978d8ed8
Bastien Nocera <hadess@hadess.net>
2018-06-15 13:22:45 +0200

    Revert "properties: Require nautilus-extension 3.28.0"
    We're about to release a 3.26.x release, so don't require 3.28.x
    components.
    
    This reverts commit b0d0b677df4b57e17a313aef292a8c1865492fcc.

--- src/totem-properties-main.c
+++ src/totem-properties-main.c
@@ -31,7 +31,8 @@
 #include <gst/gst.h>
 
 #include "totem-properties-view.h"
-#include <nautilus-extension.h>
+#include <libnautilus-extension/nautilus-extension-types.h>
+#include <libnautilus-extension/nautilus-property-page-provider.h>
 
 #define WANT_MIME_TYPES 1
 #include "totem-mime-types.h"
