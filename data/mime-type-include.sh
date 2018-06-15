cf1f15d921545d0a168024f7c6169344b4abc33a
Bastien Nocera <hadess@hadess.net>
2018-06-15 12:03:25 +0200

    data: Fix warnings about unused variables in mime-types arrays

--- data/mime-type-include.sh
+++ data/mime-type-include.sh
@@ -12,7 +12,7 @@ if [ x"$1" = "x--nautilus" ] ; then
 	echo "/* generated with mime-type-include.sh in the totem module, don't edit or"
 	echo "   commit in the nautilus module without filing a bug against totem */"
 
-	echo "static const char *video_mime_types[] = {"
+	echo "G_GNUC_UNUSED static const char *video_mime_types[] = {"
 	for i in $MIMETYPES ; do
 		echo_mime;
 	done
@@ -26,8 +26,7 @@ fi
 MIMETYPES=`grep -v '^#' $1 | grep -v x-content/ | grep -v x-scheme-handler/`
 
 echo "/* generated with mime-types-include.sh, don't edit */"
-echo "#ifdef WANT_MIME_TYPES"
-echo "static const gchar *mime_types[] = {"
+echo "G_GNUC_UNUSED static const gchar *mime_types[] = {"
 
 for i in $MIMETYPES ; do
 	echo_mime;
@@ -35,28 +34,23 @@ done
 
 echo "NULL"
 echo "};"
-echo "#endif"
 
 get_audio_mimetypes $1;
 
-echo "#ifdef WANT_AUDIO_MIME_TYPES"
-echo "static const gchar *audio_mime_types[] = {"
+echo "G_GNUC_UNUSED static const gchar *audio_mime_types[] = {"
 for i in $MIMETYPES ; do
 	echo_mime;
 done
 
 echo "NULL"
 echo "};"
-echo "#endif"
 
 get_video_mimetypes $1;
 
-echo "#ifdef WANT_VIDEO_MIME_TYPES"
-echo "static const gchar *video_mime_types[] = {"
+echo "G_GNUC_UNUSED static const gchar *video_mime_types[] = {"
 for i in $MIMETYPES ; do
 	echo_mime;
 done
 
 echo "NULL"
 echo "};"
-echo "#endif"
