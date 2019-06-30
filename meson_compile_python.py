d2d014eb59e19342e2b87c89c5ee567aae86ea85
Jordan Petridis <jpetridis@gnome.org>
2019-06-30 21:35:44 +0300

    meson_compile_python.py: call python3 for the subproccess
    Python2 is no longer available in the GNOME Sdk.

--- meson_compile_python.py
+++ meson_compile_python.py
@@ -7,7 +7,7 @@ installdir = os.environ['MESON_INSTALL_PREFIX']
 
 if not os.environ.get('DESTDIR'):
   print('Byte-compiling python modules...')
-  subprocess.call(['python', '-m', 'compileall', '-f', '-q', installdir])
+  subprocess.call(['python3', '-m', 'compileall', '-f', '-q', installdir])
 
   print('Byte-compiling python modules (optimized versions) ...')
-  subprocess.call(['python', '-O', '-m', 'compileall', '-f', '-q', installdir])
+  subprocess.call(['python3', '-O', '-m', 'compileall', '-f', '-q', installdir])
