d1910b876cd372a49f73896f65fcec838dedbb5e
Bastien Nocera <hadess@hadess.net>
2020-02-27 11:55:18 +0100

    main: Small clean up in totem_session_save()
    Make sure to pass -1 as was intended rather than 0. Note that this makes
    no difference to the called code as it checks for > 0.

--- src/totem-session.c
+++ src/totem-session.c
@@ -101,8 +101,8 @@ totem_session_save (Totem *totem)
 
 	file = get_session_file ();
 	if (!totem_playing_dvd (totem->mrl))
-		curr = bacon_video_widget_get_current_time (totem->bvw);
-	totem_playlist_save_session_playlist (totem->playlist, file, curr / 1000);
+		curr = bacon_video_widget_get_current_time (totem->bvw) / 1000;
+	totem_playlist_save_session_playlist (totem->playlist, file, curr);
 }
 
 void
