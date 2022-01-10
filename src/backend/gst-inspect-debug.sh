fa1f32f284faab9d0e1519e65ebb558ea27520a8
Bastien Nocera <hadess@hadess.net>
2022-01-10 17:43:08 +0100

    fixup! XXX gst-inspect debug

--- src/backend/gst-inspect-debug.sh
+++ src/backend/gst-inspect-debug.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-GST_DEBUG=*:5 gst-inspect-1.0 "$1" > $MESON_BUILD_ROOT/meson-logs/gst-inspect-$1-log.txt
+GST_DEBUG=*:9 gst-inspect-1.0 "$1" > $MESON_BUILD_ROOT/meson-logs/gst-inspect-$1-log.txt
