5d21478ecb61ffe995a3f6c9bc8666c993382a02
Bastien Nocera <hadess@hadess.net>
2022-03-31 14:04:11 +0200

    main: Remove UI when XInitThreads() fails
    We fixed those bugs in Xlib nearly 20 years ago, we should be out of the
    woods now.

--- src/totem.c
+++ src/totem.c
@@ -32,7 +32,6 @@
 #include <glib-object.h>
 #include <glib/gi18n.h>
 #include <gtk/gtk.h>
-#include <libhandy-1/handy.h>
 #include <locale.h>
 #include <string.h>
 
@@ -42,7 +41,6 @@
 #endif
 
 #include "totem.h"
-#include "totem-private.h"
 
 int
 main (int argc, char **argv)
@@ -56,12 +54,7 @@ main (int argc, char **argv)
 
 #ifdef GDK_WINDOWING_X11
 	if (XInitThreads () == 0)
-	{
-		gtk_init (&argc, &argv);
-		hdy_init ();
-		g_set_application_name (_("Videos"));
-		totem_object_show_error_and_exit (_("Could not initialize the thread-safe libraries."), _("Verify your system installation. Totem will now exit."), NULL);
-	}
+		g_error ("XInitThreads() failed, OS is broken. Contact your provider for support.");
 #endif
 
 	g_set_prgname ("totem");
