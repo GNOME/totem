GNOME/totem

commit d5b2ae499ef49a170fe23592ec812396ec1a815c

main: Build main window from resource

src/totem-object.c
@@ -179,10 +179,7 @@ totem_object_app_activate (GApplication *app)
 		return;
 
 	/* Main window */
-	totem->xml = totem_interface_load ("totem.ui", TRUE, NULL, totem);
-	if (totem->xml == NULL)
-		totem_object_exit (NULL);
-
+	totem->xml = gtk_builder_new_from_resource ("/org/gnome/totem/ui/totem.ui");
 	totem->bvw = BACON_VIDEO_WIDGET (gtk_builder_get_object (totem->xml, "bvw"));
 	totem->win = GTK_WIDGET (gtk_builder_get_object (totem->xml, "totem_main_window"));
 #if DEVELOPMENT_VERSION

data/meson.build
@@ -36,15 +36,6 @@ else
   message('Man page generation disabled')
 endif
 
-ui_files = files(
-  'totem.ui',
-)
-
-install_data(
-  ui_files,
-  install_dir: totem_pkgdatadir
-)
-
 ui_resources = gnome.compile_resources(
   'totemresources',
   'totem.gresource.xml',

data/totem.gresource.xml
@@ -5,5 +5,6 @@
     <file compressed="true">playlist.ui</file>
     <file compressed="true">uri.ui</file>
     <file compressed="true">shortcuts.ui</file>
+    <file compressed="true">totem.ui</file>
   </gresource>
 </gresources>

commit 3158cfb7cd5e239b14850b8228f11b4f47e6a450

main: Fix fullscreen header bar not showing up
We replaced totem_interface_load() with simply
gtk_builder_new_from_resource(), but this helper also used to run
gtk_builder_connect_signals().

Do that to fix the fullscreen header bar not showing up.

Fixes: d5b2ae4 ("main: Build main window from resource")

src/totem-object.c
@@ -221,6 +221,7 @@ totem_object_app_activate (GApplication *app)
 
 	/* Main window */
 	totem->xml = gtk_builder_new_from_resource ("/org/gnome/totem/ui/totem.ui");
+	gtk_builder_connect_signals (totem->xml, totem);
 	totem->bvw = BACON_VIDEO_WIDGET (gtk_builder_get_object (totem->xml, "bvw"));
 	totem->win = GTK_WIDGET (gtk_builder_get_object (totem->xml, "totem_main_window"));
 #if DEVELOPMENT_VERSION