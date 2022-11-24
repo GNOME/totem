8ebbd7f7a8909cec389acaebfe809ea7c5100618
Bastien Nocera <hadess@hadess.net>
2022-11-24 10:45:28 +0100

    opensubtitles: Re-fix blank dialogue (and traceback)
    Traceback (most recent call last):
      File "/usr/lib/python3.11/site-packages/gi/overrides/Gtk.py", line 85, in _builder_connect_callback
        handler, args = _extract_handler_and_args(obj_or_map, handler_name)
                        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
      File "/usr/lib64/python3.11/site-packages/gi/_gtktemplate.py", line 34, in _extract_handler_and_args
        raise AttributeError('Handler %s not found' % handler_name)
    AttributeError: Handler Gtk.Widget.hide_on_delete not found
    
    Fixes: f2ae04f ("opensubtitles: Fix blank dialogue when reopened")

--- src/plugins/opensubtitles/opensubtitles.py
+++ src/plugins/opensubtitles/opensubtitles.py
@@ -455,7 +455,7 @@ class OpenSubtitles (GObject.Object, # pylint: disable=R0902
     def _build_dialog (self):
         ui_file_path = os.path.dirname (os.path.abspath (__file__)) + sep + 'opensubtitles.ui'
         builder = Gtk.Builder.new_from_file (ui_file_path)
-        builder.connect_signals(None)
+        builder.connect_signals(self)
 
         # Obtain all the widgets we need to initialize
         combobox = builder.get_object ('language_combobox')
@@ -526,6 +526,10 @@ class OpenSubtitles (GObject.Object, # pylint: disable=R0902
                                                 self.__on_treeview__row_change)
         self._tree_view.connect ('row-activated',
                                self.__on_treeview__row_activate)
+
+    def _hide_on_delete(self, *_):
+        self._dialog.hide_on_delete()
+        return True
 
     def _show_dialog (self, *_):
         if not self._dialog:
