GNOME/totem

commit f2ae04f3bbf8196d8bed3863ba5fff27127ab049

opensubtitles: Fix blank dialogue when reopened
totem_plugin_load_interface() used to call
totem_interface_load_with_full_path() which itself called
gtk_builder_connect_signals().

And as we didn't do that anymore when we started loading the file
ourselves, gtk_widget_hide_on_delete() wasn't called so the dialogue
was destroyed which we didn't expect.

With help from Krifa75 <yahiaoui.fakhri@gmail.com>

Fixes: 2001dfe ("opensubtitles: Fix dialogue not showing up")

Closes: #549

src/plugins/opensubtitles/opensubtitles.py
@@ -455,6 +455,7 @@ class OpenSubtitles (GObject.Object, # pylint: disable=R0902
     def _build_dialog (self):
         ui_file_path = os.path.dirname (os.path.abspath (__file__)) + sep + 'opensubtitles.ui'
         builder = Gtk.Builder.new_from_file (ui_file_path)
+        builder.connect_signals(None)
 
         # Obtain all the widgets we need to initialize
         combobox = builder.get_object ('language_combobox')

commit 2a9c4e436fc9a6d4901c8f6aa51d2cf03a768d68

opensubtitles: Re-fix blank dialogue (and traceback)
Traceback (most recent call last):
  File "/usr/lib/python3.11/site-packages/gi/overrides/Gtk.py", line 85, in _builder_connect_callback
    handler, args = _extract_handler_and_args(obj_or_map, handler_name)
                    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
  File "/usr/lib64/python3.11/site-packages/gi/_gtktemplate.py", line 34, in _extract_handler_and_args
    raise AttributeError('Handler %s not found' % handler_name)
AttributeError: Handler Gtk.Widget.hide_on_delete not found

Fixes: f2ae04f ("opensubtitles: Fix blank dialogue when reopened")

src/plugins/opensubtitles/opensubtitles.py
@@ -455,7 +455,7 @@ class OpenSubtitles (GObject.Object, # pylint: disable=R0902
     def _build_dialog (self):
         ui_file_path = os.path.dirname (os.path.abspath (__file__)) + sep + 'opensubtitles.ui'
         builder = Gtk.Builder.new_from_file (ui_file_path)
-        builder.connect_signals(None)
+        builder.connect_signals(self)
 
         # Obtain all the widgets we need to initialize
         combobox = builder.get_object ('language_combobox')
@@ -526,6 +526,10 @@ class OpenSubtitles (GObject.Object, # pylint: disable=R0902
                                                 self.__on_treeview__row_change)
         self._tree_view.connect ('row-activated',
                                self.__on_treeview__row_activate)
+
+    def _hide_on_delete(self, *_):
+        self._dialog.hide_on_delete()
+        return True
 
     def _show_dialog (self, *_):
         if not self._dialog:

src/plugins/opensubtitles/opensubtitles.ui
@@ -29,7 +29,7 @@
     <property name="default_width">400</property>
     <property name="default_height">400</property>
     <property name="type_hint">dialog</property>
-    <signal name="delete-event" handler="gtk_widget_hide_on_delete" swapped="no"/>
+    <signal name="delete-event" handler="_hide_on_delete" swapped="no"/>
     <child>
       <object class="GtkBox" id="vbox1">
         <property name="orientation">vertical</property>