GNOME/totem

commit 2001dfee022cd37237f5fc3937611ed7b9c58ccb

opensubtitles: Fix dialogue not showing up
plugin_load_interface() was removed in
fd461b6571855f31cf48103a65a447c8ee3b110e and not replaced for the
opensubtitles plugin. Do that now.

src/plugins/opensubtitles/opensubtitles.py
@@ -4,6 +4,7 @@ import xmlrpc.client
 import threading
 import zlib
 from os import sep
+import os
 import gettext
 
 import gi
@@ -445,10 +446,8 @@ class OpenSubtitles (GObject.Object, # pylint: disable=R0902
     # UI related code
 
     def _build_dialog (self):
-        builder = Totem.plugin_load_interface ("opensubtitles",
-                                               "opensubtitles.ui", True,
-                                               self._totem.get_main_window (), # pylint: disable=no-member
-                                               None)
+        ui_file_path = os.path.dirname (os.path.abspath (__file__)) + sep + 'opensubtitles.ui'
+        builder = Gtk.Builder.new_from_file (ui_file_path)
 
         # Obtain all the widgets we need to initialize
         combobox = builder.get_object ('language_combobox')

commit f2ae04f3bbf8196d8bed3863ba5fff27127ab049

opensubtitles: Fix blank dialogue when reopened
totem_plugin_load_interface() used to call
totem_interface_load_with_full_path() which itself called
gtk_builder_connect_signals().

And as we didn't do that anymore when we started loading the file
ourselves, gtk_widget_hide_on_delete() wasn't called so the dialogue
was destroyed which we didn't expect.

With help from Krifa75 <yahiaoui.fakhri@gmail.com>

Fixes: 2001dfe ("opensubtitles: Fix dialogue not showing up")

Closes: #549

src/plugins/opensubtitles/opensubtitles.py
@@ -455,6 +455,7 @@ class OpenSubtitles (GObject.Object, # pylint: disable=R0902
     def _build_dialog (self):
         ui_file_path = os.path.dirname (os.path.abspath (__file__)) + sep + 'opensubtitles.ui'
         builder = Gtk.Builder.new_from_file (ui_file_path)
+        builder.connect_signals(None)
 
         # Obtain all the widgets we need to initialize
         combobox = builder.get_object ('language_combobox')