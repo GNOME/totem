9d1d87e8890ed1021ef8de8594cb529aff3f0b39
Krifa75 <yahiaoui.fakhri@gmail.com>
2023-02-16 22:24:13 +0100

    player-toolbar: Remove code related to grilo view

--- src/totem-player-toolbar.h
+++ src/totem-player-toolbar.h
@@ -15,31 +15,6 @@ G_DECLARE_FINAL_TYPE (TotemPlayerToolbar, totem_player_toolbar, TOTEM, PLAYER_TO
 
 GType           totem_player_toolbar_get_type              (void) G_GNUC_CONST;
 GtkWidget*      totem_player_toolbar_new                   (void);
-void            totem_player_toolbar_set_search_mode       (TotemPlayerToolbar *bar,
-							  gboolean          search_mode);
-gboolean        totem_player_toolbar_get_search_mode       (TotemPlayerToolbar *bar);
-void            totem_player_toolbar_set_select_mode       (TotemPlayerToolbar *bar,
-							  gboolean          select_mode);
-gboolean        totem_player_toolbar_get_select_mode       (TotemPlayerToolbar *bar);
 void            totem_player_toolbar_set_title             (TotemPlayerToolbar *bar,
 							  const char       *title);
 const char *    totem_player_toolbar_get_title             (TotemPlayerToolbar *bar);
-void            totem_player_toolbar_set_subtitle          (TotemPlayerToolbar *bar,
-							  const char       *subtitle);
-const char *    totem_player_toolbar_get_subtitle          (TotemPlayerToolbar *bar);
-void            totem_player_toolbar_set_search_string     (TotemPlayerToolbar *bar,
-						          const char       *search_string);
-const char *    totem_player_toolbar_get_search_string     (TotemPlayerToolbar *bar);
-void            totem_player_toolbar_set_n_selected        (TotemPlayerToolbar *bar,
-							  guint             n_selected);
-guint           totem_player_toolbar_get_n_selected        (TotemPlayerToolbar *bar);
-void            totem_player_toolbar_set_custom_title      (TotemPlayerToolbar *bar,
-							  GtkWidget        *title_widget);
-GtkWidget *     totem_player_toolbar_get_custom_title      (TotemPlayerToolbar *bar);
-void            totem_player_toolbar_set_select_menu_model (TotemPlayerToolbar *bar,
-							  GMenuModel       *model);
-GMenuModel *    totem_player_toolbar_get_select_menu_model (TotemPlayerToolbar *bar);
-void            totem_player_toolbar_pack_start            (TotemPlayerToolbar *bar,
-							  GtkWidget        *child);
-void            totem_player_toolbar_pack_end              (TotemPlayerToolbar *bar,
-							  GtkWidget        *child);
