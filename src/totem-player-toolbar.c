0c99bd04d86a1875ba5ab01d48538c85a0d4fd2c
Krifa75 <yahiaoui.fakhri@gmail.com>
2023-02-16 22:38:09 +0100

    player-toolbar: use GtkBin as base class instead HdyHeaderBar
    HdyHeaderBar is part of the widgets which cannot be used as a base
    class in libAdwaita.

--- src/totem-player-toolbar.c
+++ src/totem-player-toolbar.c
@@ -25,13 +25,27 @@
  */
 
 struct _TotemPlayerToolbar {
-  HdyHeaderBar parent;
+  GtkBin       parent;
+
+  GtkWidget   *header_bar;
 
   /* Template widgets */
   GtkWidget   *back_button;
+  GtkWidget   *fullscreen_button;
+  GtkWidget   *unfullscreen_button;
+
+  // Menus
+  GtkWidget   *subtitles_menu_button;
+  GtkWidget   *player_menu_button;
 };
 
-G_DEFINE_TYPE(TotemPlayerToolbar, totem_player_toolbar, HDY_TYPE_HEADER_BAR)
+G_DEFINE_TYPE(TotemPlayerToolbar, totem_player_toolbar, GTK_TYPE_BIN)
+
+enum {
+  PROP_0,
+  PROP_SUBTITLES_MENU_MODEL,
+  PROP_PLAYER_MENU_MODEL
+};
 
 static void
 back_button_clicked_cb (GtkButton        *button,
@@ -41,10 +55,77 @@ back_button_clicked_cb (GtkButton        *button,
 }
 
 static void
+totem_player_toolbar_set_property (GObject         *object,
+                                   guint            prop_id,
+                                   const GValue    *value,
+                                   GParamSpec      *pspec)
+{
+  TotemPlayerToolbar *self = TOTEM_PLAYER_TOOLBAR (object);
+
+  switch (prop_id)
+    {
+    case PROP_SUBTITLES_MENU_MODEL:
+      gtk_menu_button_set_menu_model (GTK_MENU_BUTTON (self->subtitles_menu_button), g_value_get_object (value));
+      break;
+
+    case PROP_PLAYER_MENU_MODEL:
+      gtk_menu_button_set_menu_model (GTK_MENU_BUTTON (self->player_menu_button), g_value_get_object (value));
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+    }
+}
+
+static void
+totem_player_toolbar_get_property (GObject         *object,
+                                   guint            prop_id,
+                                   GValue          *value,
+                                   GParamSpec      *pspec)
+{
+  TotemPlayerToolbar *self = TOTEM_PLAYER_TOOLBAR (object);
+
+  switch (prop_id)
+    {
+    case PROP_SUBTITLES_MENU_MODEL:
+      g_value_set_object (value, gtk_menu_button_get_menu_model (GTK_MENU_BUTTON (self->subtitles_menu_button)));
+      break;
+
+    case PROP_PLAYER_MENU_MODEL:
+      g_value_set_object (value, gtk_menu_button_get_menu_model (GTK_MENU_BUTTON (self->player_menu_button)));
+      break;
+
+    default:
+      G_OBJECT_WARN_INVALID_PROPERTY_ID (object, prop_id, pspec);
+      break;
+    }
+}
+
+static void
 totem_player_toolbar_class_init (TotemPlayerToolbarClass *klass)
 {
   GObjectClass *object_class = G_OBJECT_CLASS (klass);
   GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+
+  object_class->set_property = totem_player_toolbar_set_property;
+  object_class->get_property = totem_player_toolbar_get_property;
+
+  g_object_class_install_property (object_class,
+                                   PROP_SUBTITLES_MENU_MODEL,
+                                   g_param_spec_object ("subtitles-menu-model",
+                                                        "subtitles-menu-model",
+                                                        "The subtitles dropdown menu's model.",
+                                                        G_TYPE_MENU_MODEL,
+                                                        G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY));
+
+  g_object_class_install_property (object_class,
+                                   PROP_PLAYER_MENU_MODEL,
+                                   g_param_spec_object ("player-menu-model",
+                                                        "player-menu-model",
+                                                        "The player dropdown menu's model.",
+                                                        G_TYPE_MENU_MODEL,
+                                                        G_PARAM_READWRITE | G_PARAM_CONSTRUCT_ONLY));
 
   g_signal_new ("back-clicked",
                 G_OBJECT_CLASS_TYPE (klass),
@@ -55,6 +136,11 @@ totem_player_toolbar_class_init (TotemPlayerToolbarClass *klass)
                 G_TYPE_NONE, 0, G_TYPE_NONE);
 
   gtk_widget_class_set_template_from_resource (widget_class, "/org/gnome/totem/ui/totem-player-toolbar.ui");
+  gtk_widget_class_bind_template_child (widget_class, TotemPlayerToolbar, header_bar);
+  gtk_widget_class_bind_template_child (widget_class, TotemPlayerToolbar, fullscreen_button);
+  gtk_widget_class_bind_template_child (widget_class, TotemPlayerToolbar, unfullscreen_button);
+  gtk_widget_class_bind_template_child (widget_class, TotemPlayerToolbar, subtitles_menu_button);
+  gtk_widget_class_bind_template_child (widget_class, TotemPlayerToolbar, player_menu_button);
   gtk_widget_class_bind_template_child (widget_class, TotemPlayerToolbar, back_button);
 }
 
@@ -89,7 +175,7 @@ totem_player_toolbar_set_title (TotemPlayerToolbar *bar,
 {
   g_return_if_fail (TOTEM_IS_PLAYER_TOOLBAR (bar));
 
-  hdy_header_bar_set_title (HDY_HEADER_BAR (bar), title);
+  hdy_header_bar_set_title (HDY_HEADER_BAR (bar->header_bar), title);
 }
 
 const char *
@@ -97,5 +183,34 @@ totem_player_toolbar_get_title (TotemPlayerToolbar *bar)
 {
   g_return_val_if_fail (TOTEM_IS_PLAYER_TOOLBAR (bar), NULL);
 
-  return hdy_header_bar_get_title (HDY_HEADER_BAR (bar));
+  return hdy_header_bar_get_title (HDY_HEADER_BAR (bar->header_bar));
+}
+
+void
+totem_player_toolbar_set_fullscreen_mode (TotemPlayerToolbar *bar,
+                                          gboolean            is_fullscreen)
+{
+  g_return_if_fail (TOTEM_IS_PLAYER_TOOLBAR (bar));
+
+
+  gtk_widget_set_visible(bar->unfullscreen_button, is_fullscreen);
+  gtk_widget_set_visible(bar->fullscreen_button, !is_fullscreen);
+
+  hdy_header_bar_set_show_close_button (HDY_HEADER_BAR (bar->header_bar), !is_fullscreen);
+}
+
+GtkWidget *
+totem_player_toolbar_get_subtitles_button (TotemPlayerToolbar *bar)
+{
+  g_return_val_if_fail (TOTEM_IS_PLAYER_TOOLBAR (bar), NULL);
+
+  return bar->subtitles_menu_button;
+}
+
+GtkWidget *
+totem_player_toolbar_get_player_button (TotemPlayerToolbar *self)
+{
+  g_return_val_if_fail (TOTEM_IS_PLAYER_TOOLBAR (self), NULL);
+
+  return self->player_menu_button;
 }
--- src/totem-player-toolbar.h
+++ src/totem-player-toolbar.h
@@ -11,10 +11,14 @@
 #include <handy.h>
 
 #define TOTEM_TYPE_PLAYER_TOOLBAR                 (totem_player_toolbar_get_type ())
-G_DECLARE_FINAL_TYPE (TotemPlayerToolbar, totem_player_toolbar, TOTEM, PLAYER_TOOLBAR, HdyHeaderBar)
+G_DECLARE_FINAL_TYPE (TotemPlayerToolbar, totem_player_toolbar, TOTEM, PLAYER_TOOLBAR, GtkBin)
 
 GType           totem_player_toolbar_get_type              (void) G_GNUC_CONST;
 GtkWidget*      totem_player_toolbar_new                   (void);
 void            totem_player_toolbar_set_title             (TotemPlayerToolbar *bar,
 							  const char       *title);
 const char *    totem_player_toolbar_get_title             (TotemPlayerToolbar *bar);
+void            totem_player_toolbar_set_fullscreen_mode   (TotemPlayerToolbar *bar,
+                                                            gboolean            show_fullscreen);
+GtkWidget*      totem_player_toolbar_get_subtitles_button  (TotemPlayerToolbar *bar);
+GtkWidget*      totem_player_toolbar_get_player_button     (TotemPlayerToolbar *bar);
