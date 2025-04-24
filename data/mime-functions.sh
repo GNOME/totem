GNOME/totem

commit f7ee62408b72bf68819481e52c49921316633f61

data: Simplify removing application/smil from video mime-types
The new match covers application/smil, application/x-smil and
application/smil+xml

data/mime-functions.sh
@@ -8,7 +8,7 @@ get_audio_mimetypes ()
 
 get_video_mimetypes ()
 {
-	MIMETYPES=`grep -v '^#' $1 | grep -v x-content/ | grep -v audio | grep -v "application/x-flac" | grep -v "text/google-video-pointer" | grep -v "application/x-quicktime-media-link" | grep -v "application/smil" | grep -v "application/smil+xml" | grep -v "application/x-smil" | grep -v "application/xspf+xml" | grep -v -E 'application/[a-z-]*ogg'`
+	MIMETYPES=`grep -v '^#' $1 | grep -v x-content/ | grep -v audio | grep -v "application/x-flac" | grep -v "text/google-video-pointer" | grep -v "application/x-quicktime-media-link" | grep -v -E 'application/*smil*' | grep -v "application/xspf+xml" | grep -v -E 'application/[a-z-]*ogg'`
 	MIMETYPES="$MIMETYPES audio/x-pn-realaudio"
 }
 

commit 371b53bea080bcc25703f384a5c5f1b070abef5c

data: Fix duplicate application/x-smil mime-type
The regexp wasn't quite correct to match all three SMIL mime-type
variants.

Closes: #635

Fixes: f7ee62408b72 ("data: Simplify removing application/smil from video mime-types")

data/mime-functions.sh
@@ -8,7 +8,7 @@ get_audio_mimetypes ()
 
 get_video_mimetypes ()
 {
-	MIMETYPES=`grep -v '^#' $1 | grep -v x-content/ | grep -v audio | grep -v "application/x-flac" | grep -v "text/google-video-pointer" | grep -v "application/x-quicktime-media-link" | grep -v -E 'application/*smil*' | grep -v "application/xspf+xml" | grep -v "mpegurl" | grep -v -E 'application/[a-z-]*ogg'`
+	MIMETYPES=`grep -v '^#' $1 | grep -v x-content/ | grep -v audio | grep -v "application/x-flac" | grep -v "text/google-video-pointer" | grep -v "application/x-quicktime-media-link" | grep -v -E 'application/.*smil.*' | grep -v "application/xspf+xml" | grep -v "mpegurl" | grep -v -E 'application/[a-z-]*ogg'`
 	MIMETYPES="$MIMETYPES audio/x-pn-realaudio"
 }
 